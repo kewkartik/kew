072b2b3be9d2814bebb6500f3f9a2b08db8d2b30
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-06-27 16:59:54 +0530

    initial

---
+++ src/data/constants.ts
@@ -0,0 +1,20 @@
+export const MENU_LINKS = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "Blog",
+		path: "/posts/",
+	},
+	{
+		title: "About",
+		path: "/about/",
+	},
+];
+
+export const SOCIAL_LINKS = {
+	github: "https://github.com/KewKartik",
+	twitter: "https://twitter.com/KewKartik",
+	email: "me@example.com",
+};
